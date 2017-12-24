6a418af918a375511c45efa23102991ebd243cf2
jeongjoonsup <penny4860@gmail.com>
2017-12-24 18:55:58 +0900

    Revert "run_simulator() added"
    This reverts commit 25e7f4df1c3f99c47b44978173069dca80b3c204.

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,8 @@ void reset_simulator(uWS::WebSocket<uWS::SERVER>& ws){
   ws.send(msg.data(),msg.length(), uWS::OpCode::TEXT);
 }
 
-int run_simulator(void)
+
+int main()
 {
   uWS::Hub h;
 
@@ -44,98 +45,94 @@ int run_simulator(void)
   pid.Init(1.0, 0.00001, 0.1);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
-	// "42" at the start of the message means there's a websocket message event.
-	// The 4 signifies a websocket message
-	// The 2 signifies a websocket event
-	if (length && length > 2 && data[0] == '4' && data[1] == '2')
-	{
-	  auto s = hasData(std::string(data).substr(0, length));
-	  if (s != "") {
-		auto j = json::parse(s);
-		std::string event = j[0].get<std::string>();
-		if (event == "telemetry") {
-		  // j[1] is the data JSON object
-		  double cte = std::stod(j[1]["cte"].get<std::string>());
-		  double speed = std::stod(j[1]["speed"].get<std::string>());
-		  double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-		  double steer_value;
-		  /*
-		  * TODO: Calcuate steering value here, remember the steering value is
-		  * [-1, 1].
-		  * NOTE: Feel free to play around with the throttle and speed. Maybe use
-		  * another PID controller to control the speed!
-		  */
-		  pid.UpdateError(cte);
-		  steer_value = -pid.Kp * pid.p_error -pid.Kd * pid.d_error -pid.Ki * pid.i_error;
+    // "42" at the start of the message means there's a websocket message event.
+    // The 4 signifies a websocket message
+    // The 2 signifies a websocket event
+    if (length && length > 2 && data[0] == '4' && data[1] == '2')
+    {
+      auto s = hasData(std::string(data).substr(0, length));
+      if (s != "") {
+        auto j = json::parse(s);
+        std::string event = j[0].get<std::string>();
+        if (event == "telemetry") {
+          // j[1] is the data JSON object
+          double cte = std::stod(j[1]["cte"].get<std::string>());
+          double speed = std::stod(j[1]["speed"].get<std::string>());
+          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          double steer_value;
+          /*
+          * TODO: Calcuate steering value here, remember the steering value is
+          * [-1, 1].
+          * NOTE: Feel free to play around with the throttle and speed. Maybe use
+          * another PID controller to control the speed!
+          */
+          pid.UpdateError(cte);
+          steer_value = -pid.Kp * pid.p_error -pid.Kd * pid.d_error -pid.Ki * pid.i_error;
 
-		  if (steer_value < -1.0)
-			  steer_value = -1.0;
-		  if (steer_value > +1.0)
-			  steer_value = +1.0;
+          if (steer_value < -1.0)
+        	  steer_value = -1.0;
+          if (steer_value > +1.0)
+        	  steer_value = +1.0;
 
-		  double total_error = pid.TotalError();
-		  if (pid.step == 750)
-		  {
-			  std::cout << "\n	Step: " << pid.step << " average_error" << total_error << std::endl;
-			  reset_simulator(ws);
-		  }
+          double total_error = pid.TotalError();
+          if (pid.step == 750)
+          {
+              std::cout << "\n	==================================================================================";
+              std::cout << "\n	Step: " << pid.step << " average_error" << total_error << std::endl;
+              std::cout << "\n	==================================================================================";
+              reset_simulator(ws);
+          }
 //          // DEBUG
 //          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
-		  json msgJson;
-		  msgJson["steering_angle"] = steer_value;
-		  msgJson["throttle"] = 0.3;
-		  auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+          json msgJson;
+          msgJson["steering_angle"] = steer_value;
+          msgJson["throttle"] = 0.3;
+          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
 //          std::cout << msg << std::endl;
-		  ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-		}
-	  } else {
-		// Manual driving
-		std::string msg = "42[\"manual\",{}]";
-		ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-	  }
-	}
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
   });
 
   // We don't need this since we're not using HTTP but if it's removed the program
   // doesn't compile :-(
   h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
-	const std::string s = "<h1>Hello world!</h1>";
-	if (req.getUrl().valueLength == 1)
-	{
-	  res->end(s.data(), s.length());
-	}
-	else
-	{
-	  // i guess this should be done more gracefully?
-	  res->end(nullptr, 0);
-	}
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1)
+    {
+      res->end(s.data(), s.length());
+    }
+    else
+    {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
   });
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-	std::cout << "Connected!!!" << std::endl;
+    std::cout << "Connected!!!" << std::endl;
   });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
-	ws.close();
-	std::cout << "Disconnected" << std::endl;
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
   });
 
   int port = 4567;
   if (h.listen(port))
   {
-	std::cout << "Listening to port " << port << std::endl;
+    std::cout << "Listening to port " << port << std::endl;
   }
   else
   {
-	std::cerr << "Failed to listen to port" << std::endl;
-	return -1;
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
   }
   h.run();
-}
-
-
-int main()
-{
-	return run_simulator();
 }

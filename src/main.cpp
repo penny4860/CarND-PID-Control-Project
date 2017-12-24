f46d22bcc882274dfede0c9784f48a11efbbda8b
jeongjoonsup <penny4860@gmail.com>
2017-12-24 15:48:49 +0900

    tuning parameter test

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(2.933, 10.326, 0.493);
+  pid.Init(0.134611, 0.000270736, 3.05349);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.

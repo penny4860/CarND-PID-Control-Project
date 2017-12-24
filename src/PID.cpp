44bc333f0392a3ae91f513f22c116145eab72bf4
jeongjoonsup <penny4860@gmail.com>
2017-12-24 18:26:19 +0900

    nit

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,11 +10,11 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_) {
 
-	this->Kp = Kp;
-	this->Ki = Ki;
-	this->Kd = Kd;
+	Kp = Kp_;
+	Ki = Ki_;
+	Kd = Kd_;
 
 	p_error = 0;
 	i_error = 0;
